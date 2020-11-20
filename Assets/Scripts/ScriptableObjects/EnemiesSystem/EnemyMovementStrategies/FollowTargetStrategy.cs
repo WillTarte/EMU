25384911fd480db02c936e485e1d52f28de88dc6
Claudia <claudia.lapalme@gmail.com>
2020-11-20 17:25:24 -0500

    Almost finished gunner
    - need to fixed movement

--- Assets/Scripts/ScriptableObjects/EnemiesSystem/EnemyMovementStrategies/FollowTargetStrategy.cs
+++ Assets/Scripts/ScriptableObjects/EnemiesSystem/EnemyMovementStrategies/FollowTargetStrategy.cs
@@ -2,27 +2,25 @@
 
 namespace ScriptableObjects.EnemiesSystem.EnemyMovementStrategies
 {
-    [CreateAssetMenu(fileName = "NewFollowTargetStrategy", 
+    [CreateAssetMenu(fileName = "NewFollowTargetStrategy",
         menuName = "ScriptableObjects/EnemyMovement/Follow", order = 3)]
     public class FollowTargetStrategy : EnemyMovementStrategy
     {
-        
         #region Interface Variables
 
         [SerializeField] private float followSpeed = 4;
         [SerializeField] private float followRange = 10;
-        
+
         #endregion
-        
+
         public override void Move(Transform emuTransform, Transform playerTransform)
         {
-            if (playerTransform != null && Vector2.Distance(emuTransform.position, playerTransform.position) < followRange)
+            if (playerTransform != null &&
+                Vector2.Distance(emuTransform.position, playerTransform.position) < followRange)
             {
                 emuTransform.position =
                     Vector2.MoveTowards(emuTransform.position, playerTransform.position, followSpeed * Time.deltaTime);
             }
         }
-        
-        
     }
 }
--- Assets/Scripts/ScriptableObjects/EnemiesSystem/EnemyMovementStrategies/KeepDistanceStrategy.cs
+++ Assets/Scripts/ScriptableObjects/EnemiesSystem/EnemyMovementStrategies/KeepDistanceStrategy.cs
@@ -2,13 +2,25 @@
 
 namespace ScriptableObjects.EnemiesSystem.EnemyMovementStrategies
 {
-    [CreateAssetMenu(fileName = "KeepDistanceStrategy", 
+    [CreateAssetMenu(fileName = "KeepDistanceStrategy",
         menuName = "ScriptableObjects/EnemyMovement/KeepDistance", order = 3)]
     public class KeepDistanceStrategy : EnemyMovementStrategy
     {
+        #region Interface Variables
+
+        [SerializeField] private float minDistanceBuffer = 10;
+        [SerializeField] private float speed = 4;
+
+        #endregion
+
         public override void Move(Transform emuTransform, Transform playerTransform)
         {
-            throw new System.NotImplementedException();
+            if (Vector2.Distance(emuTransform.position, playerTransform.position) < minDistanceBuffer)
+            {
+                emuTransform.position =
+                    Vector2.MoveTowards(emuTransform.position, new Vector2(-playerTransform.position.x, 0),
+                        speed * Time.deltaTime);
+            }
         }
     }
 }
