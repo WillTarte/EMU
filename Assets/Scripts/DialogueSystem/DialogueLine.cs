e60975a423ef1a330859bfbb83d204fe7a688aa8
Edouard Gagne <tedgag@gmail.com>
2020-12-09 22:38:42 -0500

    Added dialogue system

---
+++ Assets/Scripts/DialogueSystem/DialogueLine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    using TMPro;
+    
+    public class DialogueLine : Dialogue
+    {
+        private TextMeshProUGUI textHolder;
+        [Header ("Text")]
+        [SerializeField] private string input;
+        [SerializeField] private float delay;
+        [Header ("Image")]
+        [SerializeField] private Sprite sprite;
+        [SerializeField] private Image imageHolder;
+        private void Awake()
+        {
+            textHolder = GetComponent<TextMeshProUGUI>();
+            textHolder.text = "";
+            
+            gameObject.SetActive(false);
+        }
+        
+        private void OnEnable()
+        {
+            imageHolder.sprite = sprite;
+            imageHolder.preserveAspect = true;
+            StartCoroutine(WriteText(input, textHolder, delay));
+        }
+    }
+
+}
---
+++ Assets/Scripts/DialogueSystem/DialogueTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace DialogueSystem
+{
+    
+    public class DialogueTrigger : MonoBehaviour
+    {
+        [SerializeField] private GameObject dialogue;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+
+            if (other.CompareTag("Player"))
+            {
+                dialogue.SetActive(true);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
+
