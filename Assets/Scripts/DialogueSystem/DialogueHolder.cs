d28c10e27264acc2cae830f89552c13a8adcc270
Edouard Gagne <tedgag@gmail.com>
2020-12-11 00:41:39 -0500

    Level 1 complete

--- Assets/Scripts/DialogueSystem/DialogueHolder.cs
+++ Assets/Scripts/DialogueSystem/DialogueHolder.cs
@@ -5,8 +5,9 @@ namespace DialogueSystem
 {
     public class DialogueHolder : MonoBehaviour
     {
-        private void Awake()
+        private void OnEnable()
         {
+            
             StartCoroutine(DialogueSequence());
         }
         
@@ -14,6 +15,7 @@ namespace DialogueSystem
         {
             for (int i = 0; i < transform.childCount; i++)
             {
+                
                 Deactivate();
                 transform.GetChild(i).gameObject.SetActive(true);
                 yield return new WaitUntil(() => transform.GetChild(i).GetComponent<DialogueLine>().finished);
