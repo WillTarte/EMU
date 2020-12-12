659016cfd791fd259a43f0a469654d7b23920bbe
Zlata K <zlata.098@gmail.com>
2020-12-11 19:13:37 -0500

    Added parallax background

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,32 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private Transform camera;
+    private Vector3 lastCamPos;
+    private float multiplier = 0.7f; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.FindWithTag("MainCamera").transform;
+        lastCamPos = camera.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 delta = camera.position - lastCamPos;
+        if (this.gameObject.name == "Clouds")
+        {
+            transform.position += delta * multiplier;
+        }
+        else
+        {
+            transform.position += delta;
+        }
+        lastCamPos = camera.position;
+    }
+}
